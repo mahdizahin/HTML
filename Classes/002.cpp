7f90d4d0604f9b2d8ca5a1b36510188552a815b6
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-22 14:33:50 +0600

    Add files via upload

---
+++ Classes/002.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int life=1;
+    if(life>0){
+        cout<<"I love you in every universe";
+    }else{
+        cout<<"What if love";
+    }
+   
+
+};
